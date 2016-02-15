edb0cd535003ea9311d05ac5a3f64734074ea03d
chawlie <charliechauvin@gmail.com>
2016-02-14 21:56:56 -0600

    Header, footer, home and basic subpage styling

--- wp-content/themes/GoldenRollers/templates/page-full-width.php
+++ wp-content/themes/GoldenRollers/templates/page-full-width.php
@@ -7,6 +7,13 @@ get_header(); ?>
 <?php get_template_part( 'parts/featured-image' ); ?>
 
 <div id="page-full-width" role="main">
+  <header class="subpage-hero">
+    <div class="row">
+      <div class="logo">
+        <img src="<?php echo get_bloginfo('template_directory');?>/assets/images/golden_rollers_logo.png" alt="Golden Rollers" title="Golden Rollers">
+      </div>
+    </div>
+  </header>
 
 <?php do_action( 'foundationpress_before_content' ); ?>
 <?php while ( have_posts() ) : the_post(); ?>
@@ -14,17 +21,19 @@ get_header(); ?>
       <header>
           <h1 class="entry-title"><?php the_title(); ?></h1>
       </header>
-      <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-      <div class="entry-content">
-          <?php the_content(); ?>
-      </div>
-      <footer>
-          <?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
-          <p><?php the_tags(); ?></p>
-      </footer>
-      <?php do_action( 'foundationpress_page_before_comments' ); ?>
-      <?php comments_template(); ?>
-      <?php do_action( 'foundationpress_page_after_comments' ); ?>
+      <section class="page-content">
+        <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
+        <div class="entry-content">
+            <?php the_content(); ?>
+        </div>
+        <footer>
+            <?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
+            <p><?php the_tags(); ?></p>
+        </footer>
+        <?php do_action( 'foundationpress_page_before_comments' ); ?>
+        <?php comments_template(); ?>
+        <?php do_action( 'foundationpress_page_after_comments' ); ?>
+      </section>
   </article>
 <?php endwhile;?>
 
